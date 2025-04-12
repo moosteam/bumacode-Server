70b2912185b943ae61dd07cb0d7ec509545c0bd0
huise0ng <shinhuiseong07@gmail.com>
2025-04-12 12:05:29 +0900

    feat :: swggerui 설절ㅇ

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('딸깍 (bumacode)')
+    .setDescription('[@https://github.com/moosteam/bumacode-Server](https://github.com/moosteam/bumacode-Server)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
